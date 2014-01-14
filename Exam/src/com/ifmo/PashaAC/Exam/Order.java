0bcb8201e70a971677f632bad0353bee8ff8ef09
pasha26007 <pasha26007@mail.ru>
2014-01-14 18:15:35 +0400

    Асадчий Павел
    гр. 2539

---
+++ Exam/src/com/ifmo/PashaAC/Exam/Order.java
@@ -0,0 +1,19 @@
+package com.ifmo.PashaAC.Exam;
+
+public class Order {
+    String mark;
+    String colour;
+    String auto_number;
+    String time;
+    String telephone;
+    String box;
+
+    public Order(String mark, String colour, String auto_number, String time, String telephone, String box) {
+        this.mark = mark;
+        this.colour = colour;
+        this.auto_number = auto_number;
+        this.time = time;
+        this.telephone = telephone;
+        this.box = box;
+    }
+}
